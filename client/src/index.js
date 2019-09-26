5772e698cf37b9424325cfb2f2d4fe4afd0cc8d8
Tray Fleary <trayfleary@Trays-MacBook-Pro.local>
2019-09-26 14:20:26 -0400

    built out rest of simple functionality

--- client/src/index.js
+++ client/src/index.js
@@ -41,5 +41,99 @@ class StopwatchTime extends Component {
       );
     }
   }
+
+
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        seconds: '00',
+        value: '',
+        isClicked : false
+      }
+    //   this.secondsRemaining;
+    //   this.intervalHandle;
+      this.handleChange = this.handleChange.bind(this);
+      this.startCountDown = this.startCountDown.bind(this);
+      this.tick = this.tick.bind(this);
+    }
    
+    handleChange(event) {
+      this.setState({
+        value: event.target.value
+      })
+    }
+   
+    tick() {
+      var min = Math.floor(this.secondsRemaining / 60);
+      var sec = this.secondsRemaining - (min * 60);
+   
+      this.setState({
+        value: min,
+        seconds: sec,
+      })
+   
+      if (sec < 10) {
+        this.setState({
+          seconds: "0" + this.state.seconds,
+        })
+   
+      }
+   
+      if (min < 10) {
+        this.setState({
+          value: "0" + min,
+        })
+   
+      }
+   
+      if (min === 0 & sec === 0) {
+        clearInterval(this.intervalHandle);
+      }
+   
+   
+      this.secondsRemaining--
+    }
+   
+    startCountDown() {
+      this.intervalHandle = setInterval(this.tick, 1000);
+      let time = this.state.value;
+      this.secondsRemaining = time * 60;
+      this.setState({
+        isClicked : true
+      })
+    }
+   
+    render() {
+      const clicked = this.state.isClicked;
+      if(clicked){
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <Stopwatch value={this.state.value} seconds={this.state.seconds} />
+             
+            </div>
+          </div>
+        </div>
+      );
+      }else{
+        return (
+          <div>
+            <div className="row">
+              <div className="col-md-4"></div>
+              <div className="col-md-4">
+                <StopwatchTime value={this.state.value} handleChange={this.handleChange} />
+                <Stopwatch value={this.state.value} seconds={this.state.seconds} />
+                <Start startCountDown={this.startCountDown} value={this.state.value} />
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+  }
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
   
